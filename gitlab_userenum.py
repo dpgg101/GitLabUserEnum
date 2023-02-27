75deb2d9caa91b5cccc58f7c3d409a6e36691fa0
dpgg <d.g.23345@gmail.com>
2023-02-27 18:35:45 +0200

    Create gitlab_userenum.py

---
+++ gitlab_userenum.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import requests
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='GitLab User Enumeration')
+    parser.add_argument('--url', '-u', type=str, required=True, help='The URL of the GitLab\'s instance')
+    parser.add_argument('--wordlist', '-w', type=str, required=True, help='Path to the username wordlist')
+    args = parser.parse_args()
+
+    print('GitLab User Enumeration in python')
+
+    with open(args.wordlist, 'r') as f:
+        for line in f:
+            username = line.strip()
+            http_code = requests.head(f'{args.url}/{username}').status_code
+            if http_code == 200:
+                print(f'[+] The username {username} exists!')
+            elif http_code == 0:
+                print('[!] The target is unreachable.')
+                break
+
+if __name__ == '__main__':
+    main()
